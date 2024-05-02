a61f0a5a76b78990a77971246e694f49eb4d636e
SethanJoseph <99520487+SethanJoseph@users.noreply.github.com>
2024-05-01 20:31:53 -0700

    Create additional_hidden_ingredients.js

---
+++ additional_hidden_ingredients.js
@@ -0,0 +1,22 @@
+addIngredient("thought",{
+    color:"#E2E2E2",
+    type:"salt",
+    shape:"thought_balloon",
+    hidden:true,
+    adj:"sapient"
+});
+
+addIngredient("fire",{
+    color:"#F18133",
+    type:"crab",
+    shape:"flame",
+    hidden:true,
+    adj:"flaming"
+});
+
+addIngredient("scp-409",{
+    color:"#CBD8D9",
+    type:"rhubarb",
+    shape:"crystal",
+    hidden:true,
+});
