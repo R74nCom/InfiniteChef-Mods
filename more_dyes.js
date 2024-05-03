9ac17b186f9f9ee8fbc87bbfdc793a7f0d18eb28
ArkanMachmudAlfatih <166457857+ArkanMachmudAlfatih@users.noreply.github.com>
2024-05-03 15:54:25 +0700

    Create more_dyes.js

---
+++ more_dyes.js
@@ -0,0 +1,62 @@
+addTool("red_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it red!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("orange_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it orange!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("yellow_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it yellow!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("green_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it green!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("blue_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it blue!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("purple_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it purple!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
+
+addTool("pink_dyer", {
+    func: function(placed) { placed.h = 0; },
+    onSelect: function() { alert("Use on an ingredient to dye it pink!"); },
+    whileOn: function() { console.log("Dyer selected."); },
+    onDeselect: function() { alert("Goodbye!"); },
+    onClick: function(x,y) { console.log(x,y) },
+    shape:"liquid",
+    spin: true
