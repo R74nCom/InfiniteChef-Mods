dc6f1137ed8588b393a2b358006f6c3358835b82
HACKERPRO908 <130792075+HACKERPRO908@users.noreply.github.com>
2024-05-03 07:51:54 +0100

    Create crystals.js

---
+++ crystals.js
@@ -0,0 +1,8 @@
+addIngredient("diamond", {
+        shape:"rock",
+        color:["#34c9e0","#34aae0","#32b8c2"],
+        glow:"#32b8c2",
+        hidden:true,
+        keywords:"rock,crystal,diamond,diamonds",
+    }
+);
