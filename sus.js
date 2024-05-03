fca097c9cf3d66f641332f33564d6a02a1dd8f85
P!neapple <160773712+PineappleMod@users.noreply.github.com>
2024-05-03 20:56:13 +0300

    Update sus.js

--- sus.js
+++ sus.js
@@ -1,8 +1,32 @@
 addIngredient('amogus',{
   name: 'amogus',
-  keywords: 'sus,imposter',
+  keywords: 'sus,imposter,sussy',
   shape: 'sus',
   color: '#ff0000',
   placedShape: 'sus',
+});
+addIngredient('cheesy_sussy',{
+  name: 'cheesy',
+  keywords: 'sus,imposter,cheesy,sussy',
+  shape: 'sus',
+  color: '#FBDB65',
+  placedShape: 'sus',
+});
+addIngredient("rubber sussy", {
+    shape:"sus",
+    color:"#eb3489",
+    adj:"bouncy",
+    meltPoint:"50",
+    meltInto:"melted_sussy"
+});
+addIngredient("melted_sussy", {
+    name:"Melted sussy",
+    shape:"liquid",
+    behavior:1,
+    color:"#eb3489",
+    adj:"elastic",
+    hidden:true,
+    freezePoint:"-10",
+    freezeInto:"amogus"
 });
 addRecipe("amogus+cheese","cheesy amongus")
