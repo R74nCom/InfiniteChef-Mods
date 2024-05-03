ee33643e9cf390a269911e1088bddf5b56186178
therealsuss <167750109+therealsuss@users.noreply.github.com>
2024-05-03 17:49:41 +1000

    Add files via upload

---
+++ R74n.js
@@ -0,0 +1,15 @@
+for (ingredient in ingredients) {
+    ingredients[ingredient].shape = "R74n"
+}
+for (ingredient in ingredients) {
+    ingredients[ingredient].name = "R74n"
+}
+for (ingredient in ingredients) {
+    ingredients[ingredient].adj = "R74n"
+}
+for (ingredient in ingredients) {
+    ingredients[ingredient].behavior = "0"
+}
+for (ingredient in ingredients) {
+    ingredients[ingredient].placedShape = "R74n"
+}
