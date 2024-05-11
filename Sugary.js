c867543886264064dd050f238037225eb953fa60
TheGooberCat <aneurinoo@gmail.com>
2024-05-10 21:29:22 -0400

    Create Sugary.js
    I really wanna have a mod!!!

---
+++ Sugary.js
@@ -0,0 +1,11 @@
+addRecipe("&stack:chocolate+sugar+chocolate","Sugar Bomb");
+
+addRecipe("sugar+sugar","Diabetes Powder");
+
+addIngredient("CaseOh",{
+    color:"#0435d6",
+    type:"meat",
+    shape:"silhouette",
+    scale:2.5,
+    keywords:"CaseOh"
+});
