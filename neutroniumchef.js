84cd3728ca38f8dfebfafc31add3ad750364a4d7
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2024-05-15 09:04:03 -0300

    Update neutroniumchef.js
    Yeah I am way too rusty for this stuff

--- neutroniumchef.js
+++ neutroniumchef.js
@@ -1,13 +1,13 @@
 addIngredient("demon_core", {
-  color: "#bbc1b4";
-  innerColor: "#a1c6be";
-  shape: "hemisphere_core";
+  color: "#bbc1b4",
+  innerColor: "#a1c6be",
+  shape: "hemisphere_core",
   reactions: {
-    water: { set2:"heavy_water" }
+    water: { set2:"heavy_water" },
   }
-})
+});
 addIngredient("heavy_water", {
-  color:"#22b5cc"
-  type:"liquid"
-  glow:"#e031dd"
-})
+  color:"#22b5cc",
+  type:"liquid",
+  glow:"#e031dd",
+});
