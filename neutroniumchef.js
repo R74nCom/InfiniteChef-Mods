a35c769286c3f3ce046c9c1135e423882dca1e34
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2024-05-15 09:00:49 -0300

    Update neutroniumchef.js
    realized that my stupid ass forgot parentheses

--- neutroniumchef.js
+++ neutroniumchef.js
@@ -5,9 +5,9 @@ addIngredient("demon_core", {
   reactions: {
     water: { set2:"heavy_water" }
   }
-}
+})
 addIngredient("heavy_water", {
   color:"#22b5cc"
   type:"liquid"
   glow:"#e031dd"
-}
+})
