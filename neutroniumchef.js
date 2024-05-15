89ef47a7e6ddf1d83e32f3482fb667a458e4c2dd
StellarX20 <97905447+StellarX20@users.noreply.github.com>
2024-05-15 09:10:12 -0300

    Update neutroniumchef.js
    Added tritiated water, made demon core glow

--- neutroniumchef.js
+++ neutroniumchef.js
@@ -2,12 +2,21 @@ addIngredient("demon_core", {
   color: "#bbc1b4",
   innerColor: "#a1c6be",
   shape: "hemisphere_core",
+  glow:"#e031dd",
   reactions: {
-    water: { set2:"heavy_water" },
+    water: { set2:"tritiated_water" }
   }
 });
-addIngredient("heavy_water", {
-  color:"#22b5cc",
+addIngredient("tritiated_water", {
+  color:"#31e0b7",
   type:"liquid",
   glow:"#e031dd",
+  adj: "radioactive"
+});
+addIngridient ("heavy_water", {
+  color:"#22b5cc",
+  type:"liquid",
+  reactions: {
+    demon_core: { set1: "tritiated_water" }
+  }
 });
