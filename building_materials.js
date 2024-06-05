24261011854090b193d8c97974ddeee1e23dfbc7
HexGaming <74262333+hextheslime@users.noreply.github.com>
2024-06-05 15:24:42 +0200

    Initial Version
    Adds a bunch of building materials such as concrete, bricks and liquid death.

---
+++ building_materials.js
@@ -0,0 +1,56 @@
+addIngredient("concrete",{
+    type:"thick_liquid",
+    color:"#888888",
+    keywords:"concrete",
+	adj:"concrete"
+});
+
+addIngredient("hardened_concrete",{
+    type:"bouillon_cube",
+    color:"#444444",
+    keywords:"concrete",
+	adj:"hardened"
+});
+
+addIngredient("metal",{
+    type:"bouillon_cube",
+    color:"#BBBBBB",
+    keywords:"metal,iron",
+	adj:"metallic"
+});
+
+addIngredient("brick",{
+    type:"chocolate",
+    color:"#FF866E",
+    keywords:"brick",
+	adj:"bricky ",
+	meltInto: null
+});
+
+addIngredient("hex",{
+    type:"decor",
+    color:"#0088FF",
+    keywords:"hex,developer",
+	adj:"hexagonical ",
+});
+
+addIngredient("liquid_death",{
+    type:"salsa",
+    color:"#FF0000",
+    keywords:"death,poison",
+	adj:"deadly ",
+});
+
+addIngredient("gas_death",{
+    type:"smoke",
+    color:"#FF0000",
+    keywords:"death,poison",
+	adj:"deadly ",
+});
+
+addIngredient("cubical_death",{
+    type:"bouillon_cube",
+    color:"#FF0000",
+    keywords:"death,poison",
+	adj:"deadly ",
+});
