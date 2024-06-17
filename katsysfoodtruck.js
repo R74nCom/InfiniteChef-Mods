7151972b728cbc6e0ef21a1da87159b8b693f6bb
katsyartsy <angelsworld613@gmail.com>
2024-06-16 23:29:52 -0400

    Create katsysfoodtruck.js
    random mods i made for absolutely no reason at all!!!!!!!!11

---
+++ katsysfoodtruck.js
@@ -0,0 +1,99 @@
+// wowwowwow this is my first ever github mod
+// hey guys?!?!
+// are some of these edible? no. are they cookable? yes!!!11
+
+
+addIngredient("ice_pop", {
+    color: "#5351FF",
+    shape: "popsicle",
+    type: "other",
+    innerColor: "#1814FF",
+    stackShape: "rod_to_thick"
+    broken: "juice"
+    keywords: "popsicle,ice,cold,sweet"
+});
+
+addIngredient("vomit", {
+    color: "#608A5A",
+    shape: "splat",
+    type: "oil",
+    innerColor: "#1814FF",
+    stackShape: "liquid_splat"
+});
+
+addIngredient("bottle", {
+    color: "#22515F",
+    shape: "bottle_ridged",
+    type: "other",
+    innerColor: "##9BE8FF",
+    broken: "mircoplastics"
+    keywords: "bottle,water,cap,plastic,container"
+});
+
+addIngredient('mircoplastics", {
+    color: "#7E878A",
+    shape: "powder_rough",
+    type: "powder",
+});
+
+  addIngredient("soap", {
+    color: "#DAF7FF",
+    shape: "rectangle",
+    type: "other",
+    stackShape: "rectangle_thin"
+    broken: "soap_powder"
+    keywords: "clean"
+ reactions: {
+        water: { set1:"bubbles" },
+});
+
+ addIngredient("soap_powder", {
+    color: "#DAF7FF",
+    shape: "powder",
+    type: "powder",
+ reactions: {
+        water: { set1:"bubbles" },
+});
+
+ addIngredient("bubbles", {
+    color: "#AFEDFF",
+    shape: "dots",
+    type: "gas",
+});
+
+addIngredient("gumdrop", {
+    color: "#ff0000",
+    shape: "flat_bottom",
+    type: "jelly",
+    broken: "gelatin"
+    keywords: "candy,gummy,sugar,sweet"
+});
+
+addIngredient("gummy_worm", {
+    color: "#ff0000",
+    shape: "worm",
+    type: "jelly",
+    broken: "gelatin"
+    keywords: "candy,gummy,sugar,sweet"
+  reactions: {
+        citric_acid: { set1:"sour_gummy_worm" },
+});
+
+addIngredient("sour_gummy_worm", {
+    color: "#ff0000",
+    shape: "worm",
+    type: "jelly",
+    broken: "gelatin"
+    hidden: "true"
+    keywords: "candy,gummy,sugar,sweet"
+});
+
+addIngredient("paper", {
+    color: "#ffffff",
+    shape: "wavy_sheet_flat",
+    type: "other"
+    stackShape: "rectangle_thinnest"
+    broken: "powder"
+});
+
+
