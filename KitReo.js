5ed367ccfd3a577dace29754757d7b4cfe967502
ASimpIeCoder <nn0663228@gmail.com>
2024-08-01 01:14:05 +0330

    Create KitReo.js

---
+++ KitReo.js
@@ -0,0 +1,112 @@
+//more flavors and recipes soon!
+
+
+addIngredient("Oreo",{
+type:"candy", 
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#291d13",
+});
+
+addIngredient("KitKat",{
+type:"candy",
+brokenShape:"squares_some_flat",
+shape:"star_prism",
+color:"#561b09",
+});
+
+addIngredient("Green Tea KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#bacb8d",
+});
+
+addIngredient("Strawberry KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#f6afb3",
+});
+
+addIngredient("Golden Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#b17a16",
+});
+
+addIngredient("Mint Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#a2ca96",
+
+});
+
+addIngredient("Strawberry Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#dc9ca6",
+});
+
+addIngredient("Melon Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#84bb42",
+});
+
+addIngredient("Watermelon Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#c70124",
+});
+
+addIngredient("Mint KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#95c08a",
+});
+
+addIngredient("Pumpkin Pie KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#e07d22",
+});
+
+addIngredient("Ruby Cocoa KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#d1628e",
+});
+
+addIngredient("Mango Oreo",{
+type:"candy",
+shape:"disc_hole",
+brokenShape:"squares_some_flat",
+color:"#d39635",
+});
+
+addIngredient("Mango KitKat",{
+type:"candy",
+shape:"star_prism",
+brokenShape:"squares_some_flat",
+color:"#f6d221",
+});
+
+addRecipe("kitkat+green_tea_kitkat+strawberry_kitkat+mint_kitkat+pumpkin_pie_kitkat+ruby_cocoa_kitkat+mango_kitkat+oreo+golden_oreo+mint_oreo+strawberry_oreo+melon_oreo+watermelon_oreo+mango_oreo","The Holy KitReo");
+
+addRecipe("oreo+golden_oreo+mint_oreo+strawberry_oreo+melon_oreo+watermelon_oreo+mango_oreo","The Holy Oreo");
+
+
+addRecipe("oreo+kitkat","OreKat");
+
+addRecipe("kitKat+oreo","KitReo");
+
+addRecipe("kitkat+green_tea_kitkat+strawberry_kitkat+mint_kitkat+pumpkin_pie_kitkat+ruby_cocoa_kitkat+mango_kitkat","The Holy Kitkat");
