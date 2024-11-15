e8b0a02300a50e3757717da6e3e3593a9ecf7aa6
aligithub12-zzz <ali1049944@gmail.com>
2024-11-15 19:18:00 +0400

    Create red_ball.js

---
+++ red_ball.js
@@ -0,0 +1,10 @@
+addIngredient("red_ball", {
+        shape:"rock_ball",
+        color:["#ff0000"],
+        hidden:false
+    }
+);
+
+addRecipe("red_ball+salt","rellted ball?!");
+
+addRecipe("=red_ball","lonely red ball...");
