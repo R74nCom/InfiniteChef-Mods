4844080f2e9c9d39864be4691abe007fb5c77839
Rick-300 <157910615+Rick-300@users.noreply.github.com>
2025-02-02 12:42:03 -0500

    Pull off a mekanism reference and a biomancy reference at the same time

---
+++ nutrients.js
@@ -0,0 +1,22 @@
+// mekanism
+
+addIngredient("nutritional_paste",{
+    color:"#d66093",
+    type:"liquid",
+    keywords:"nutrient"
+});
+
+// biomancy
+
+addIngredient("nutritional_mush",{
+    color:"#929659",
+    type:"thick_liquid",
+    shape:"blob_short",
+    keywords:"nutrient,paste"
+});
+
+addIngredient("nutrient_bar",{
+    color:"#929659",
+    shape:"rectangle_thin",
+    keywords:"nutrient"
+});
