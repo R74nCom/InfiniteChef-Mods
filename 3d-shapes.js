a639ff12c0f35d8b70c50e019a99899f55d5fcf8
TruncatedDodecahedron <jnfrontrow2020@gmail.com>
2025-06-16 20:49:28 +0800

    Create 3d-shapes.js

---
+++ 3d-shapes.js
@@ -0,0 +1,19 @@
+//just 3d shapes.
+
+addIngredient("3dtorus",{
+type:"shape",
+shape:"torus",
+brokenShape:"squares_some_flat",
+color:"#291d13",
+adj:"oreo"
+});
+
+addIngredient("disc_candy",{
+type:"candy",
+shape:"disc",
+brokenShape:"squares_some_flat",
+color:"#291d13",
+adj:"oreo"
+});
+
+//just 2 of them, more coming soon
