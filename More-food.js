6d4f8a74051e87f2565562b1255454093311b79a
Fenicf0xy <patti.palmieri@icloud.com>
2026-01-06 16:23:02 -0500

    Update More-food.js
    Cocoa Powder, (ingredient)
    Hot Cocoa, (ingredient)
    Candy Cane, (ingredient)
    Peppermint Hot Cocoa (recipe)

--- More-food.js
+++ More-food.js
@@ -24,10 +24,33 @@ addIngredient("soft_ramen",{
     stackShape:"foliage_bar"
 });
 
+addIngredient("candy_cane", {
+    color:"#ff0000ff",
+    innerColor:"#ffffffff",
+    shape:"cane_striped"
+});
 
+addIngredient("cocoa_powder",{
+    color:"#836333ff",
+    shape:"dots_some_flat",
+    reactions: {
+        water: {
+            set1: "hot_cocoa",
+            tempMin: 90
+        }
+    }
+});
+
+addIngredient("hot_cocoa",{
+    color:"#836333ff",
+    shape:"liquid",
+    type:"liquid",
+    stackShape:"liquid_splat"
+});
 
 
 addRecipe("soft_ramen+soy_sauce","soy ramen noodles");
 
 addRecipe("hard_ramen+soy_sauce","soy crunchy ramen noodles");
 
+addRecipe("hot_cocoa+candy_cane", "peppermint hot cocoa");
