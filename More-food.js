642b9c2e2cee8209df4b1bc219ac40efa20077c3
Fenicf0xy <patti.palmieri@icloud.com>
2026-01-07 09:41:17 -0500

    Update More-food.js
    Tea bag (ingredient)
    Honeycomb (ingredient)
    Jello (ingredient)
    Added keywords
    Tea (recipe)
    Iced tea (recipe)

--- More-food.js
+++ More-food.js
@@ -2,7 +2,8 @@
 addIngredient("pork_roll",{
     color:"#f27373ff", 
     type:"meat",
-    shape:"puddle"
+    shape:"puddle",
+    keywords: "meat, pork, ham"
 });
 
 addIngredient("hard_ramen",{
@@ -14,20 +15,23 @@ addIngredient("hard_ramen",{
             set1: "soft_ramen",
             tempMin: 90
         }
-    }
+    },
+    keywords: "noodle, ramen"
 });
 
 addIngredient("soft_ramen",{
     color:"#e9e952ff",
     type:"noodle",
     shape:"algae",
-    stackShape:"foliage_bar"
+    stackShape:"foliage_bar",
+    keywords: "noodle, ramen"
 });
 
 addIngredient("candy_cane", {
     color:"#ff0000ff",
     innerColor:"#ffffffff",
-    shape:"cane_striped"
+    shape:"cane_striped",
+    keywords: "candy, sweet, cane"
 });
 
 addIngredient("cocoa_powder",{
@@ -38,19 +42,43 @@ addIngredient("cocoa_powder",{
             set1: "hot_cocoa",
             tempMin: 90
         }
-    }
+    },
+    keywords: "chocolate, cocoa, powder"
 });
 
 addIngredient("hot_cocoa",{
     color:"#836333ff",
     shape:"liquid",
     type:"liquid",
-    stackShape:"liquid_splat"
+    stackShape:"liquid_splat",
+    keywords: "chocolate, cocoa"
 });
 
+addIngredient("jello",{
+    color:"ff000080",
+    shape:"bundt",
+    keywords: "jelly, jello"
+});
+
+addIngredient("honeycomb",{
+    color:"#fcb035ff",
+    innercolor:"#e69f2eff",
+    shape:"honeycomb",
+    keywords: "honey, bee"
+});
+
+addIngredient("tea_bag",{
+    color:"#915f0d",
+    shape:"cylinder_flat",
+    keywords:"tea, bag"
+});
 
 addRecipe("soft_ramen+soy_sauce","soy ramen noodles");
 
 addRecipe("hard_ramen+soy_sauce","soy crunchy ramen noodles");
 
 addRecipe("hot_cocoa+candy_cane", "peppermint hot cocoa");
+
+addRecipe("tea_bag+water", "tea");
+
+addRecipe("tea_bag+water+ice", "iced tea);
