66c2590597d9c60bcb1c1e856eb90a4be10d7bef
Fenicf0xy <patti.palmieri@icloud.com>
2026-01-07 15:29:32 -0500

    Update More-food.js
    Dooritos (ingredient)
    Doorito dust (ingredient)
    Jello liquid (ingredient)
    Jello powder (ingredient)
    Jello chunks (ingredient)
    Doorito (recipe)

--- More-food.js
+++ More-food.js
@@ -6,7 +6,7 @@ addIngredient("pork_roll",{
     keywords: "meat, pork, ham"
 });
 
-addIngredient("hard_ramen",{
+addIngredient("ramen",{
     color:"#f0f07dff",
     type:"noodle",
     shape:"noodles",
@@ -24,7 +24,9 @@ addIngredient("soft_ramen",{
     type:"noodle",
     shape:"algae",
     stackShape:"foliage_bar",
-    keywords: "noodle, ramen"
+    keywords: "noodle, ramen",
+    dishName: "ramen",
+    hidden:true
 });
 
 addIngredient("candy_cane", {
@@ -39,8 +41,7 @@ addIngredient("cocoa_powder",{
     shape:"dots_some_flat",
     reactions: {
         water: {
-            set1: "hot_cocoa",
-            tempMin: 90
+            set1: "hot_cocoa"
         }
     },
     keywords: "chocolate, cocoa, powder"
@@ -51,13 +52,17 @@ addIngredient("hot_cocoa",{
     shape:"liquid",
     type:"liquid",
     stackShape:"liquid_splat",
-    keywords: "chocolate, cocoa"
+    keywords: "chocolate, cocoa",
+    hidden:true
 });
 
 addIngredient("jello",{
     color:"ff000080",
     shape:"bundt",
-    keywords: "jelly, jello"
+    keywords: "jelly, jello",
+    broken:"jello_chunks",
+    meltPoint:"80",
+    meltInto:"jello_liquid"
 });
 
 addIngredient("honeycomb",{
@@ -70,7 +75,62 @@ addIngredient("honeycomb",{
 addIngredient("tea_bag",{
     color:"#915f0d",
     shape:"cylinder_flat",
-    keywords:"tea, bag"
+    keywords:"tea, bag",
+    broken:"tea_leaf"
+});
+
+addIngredient("jello_chunks",{
+    color:"#ff0000ff",
+    shape:"dots_some_flat",
+    keywords:"jello, jelly",
+    meltPoint:"80",
+    meltInto:"jello_liquid"
+});
+
+addIngredient("jello_liquid",{
+    color:"#ff0000ff",
+    shape:"liquid",
+    type:"liquid",
+    keywords:"jello, jelly, liquid",
+    meltPoint:"100",
+    meltInto:"ready_jello_liquid",
+    hidden:true
+});
+
+addIngredient("ready_jello_liquid",{
+    color:"#ff4343ff",
+    shape:"liquid",
+    type:"liquid",
+    keywords:"jello, jelly",
+    hidden:true,
+    freezePoint:"-10",
+    freezeInto:"jello",
+    dishName:"jello_liquid"
+});
+
+addIngredient("jello_powder",{
+    color:"#fb6b6bff",
+    shape:"dots_some",
+    keywords:"jello, jelly",
+    reactions:{
+        water:{
+            set1:"jello_liquid"
+        }
+    }
+});
+
+addIngredient("doorito",{
+    color:"#f66707ff",
+    shape:"triangle_round",
+    keywords:"dorito, chip, junk food",
+    broken:"doorito_dust"
+});
+
+addIngredient("doorito_dust",{
+    color:"#f66707ff",
+    shape:"dots_some",
+    keywords:"dorito, chip, junk food",
+    hidden:true
 });
 
 addRecipe("soft_ramen+soy_sauce","soy ramen noodles");
@@ -81,4 +141,6 @@ addRecipe("hot_cocoa+candy_cane", "peppermint hot cocoa");
 
 addRecipe("tea_bag+water", "tea");
 
-addRecipe("tea_bag+water+ice", "iced tea);
+addRecipe("tea_bag+water+ice", "iced tea");
+
+addRecipe("tortia_chip+doorito_dust", "doorito");
