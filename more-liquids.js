0d878d3305933087aa3268376c7592c9d02bd7a9
Richard A <67555850+Gfdcvn@users.noreply.github.com>
2026-01-13 17:18:33 +0100

    Add new liquid ingredients and recipes

--- more-liquids.js
+++ more-liquids.js
@@ -4,7 +4,21 @@ addIngredient("aloe_water",{
     type:"liquid",
     color:"#d9ffce",
 });
+addIngredient("hibiscus_extract",{
+    type:"liquid",
+    color:"#8B0000",
+});
+addIngredient("carrot_juice",{
+    type:"liquid",
+    color:"#ffd17a",
+});
+addIngredient("rhubarb_juice",{
+    type:"liquid",
+    color:"#E34253",
+});
 
 
-
-addRecipe("apple+peach?+sugar","fruit flavoured water");
+addRecipe("aloe_water+apple?+peach","fruit flavoured water");
+addRecipe("hibiscus_extract+honey?+ice_cube","Hibiscus Iced Tea");
+addRecipe("carrot_juice+ginger_root+garlic+broth","Ginger Carrot Soup");
+addRecipe("rhubarb_juice+apple+peach+honey","Rhubarb Smoothie");
